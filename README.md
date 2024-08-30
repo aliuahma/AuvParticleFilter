455ae880d9027b08f289ff5c6d031c4c4b2cd139
AliAhmad810 <hiimaliahmad@gmail.com>
2024-08-30 08:35:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AUV Particle Filter
+Particle filter algorithm with simulated dynamics and animations for autonomous underwater vehicles
---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Particle Filter Localization
+
+author: Ali Ahmad
+"""
+
+import sys
+import pathlib
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
